6af76dc4b1a30fed23f7198a2a0835f71d039adf
harryredman06@gmail.com <68497855+hazzaRR@users.noreply.github.com>
2024-03-05 16:58:55 +0000

    updated the return type for all the methods in matchResult service to return a task<T> since i am working with async await with a postgres database

--- Interfaces/IMatchResultService.cs
+++ Interfaces/IMatchResultService.cs
@@ -11,62 +11,62 @@ namespace DerbyCountyAPI.Interfaces
 
         string GetCurrentSeason();
 
-        List<MatchResult> GetMatchResultsBySeasonAndCompetitionAndStadiumAndTeamAndResult(String season, String competition, String stadium, String team, String result);
+        Task<List<MatchResult>> GetMatchResultsBySeasonAndCompetitionAndStadiumAndTeamAndResult(String season, String competition, String stadium, String team, String result);
 
-        List<MatchResult> GetMatchResultsBySeasonAndCompetitionAndStadiumAndTeam(String season, String competition, String stadium, String team);
+        Task<List<MatchResult>> GetMatchResultsBySeasonAndCompetitionAndStadiumAndTeam(String season, String competition, String stadium, String team);
 
-        List<MatchResult> GetMatchResultsBySeasonAndCompetitionAndStadium(String season, String competition, String stadium);
+        Task<List<MatchResult>> GetMatchResultsBySeasonAndCompetitionAndStadium(String season, String competition, String stadium);
 
-        List<MatchResult> GetMatchResultsBySeasonAndCompetitionAndTeam(String season, String competition, String team);
+        Task<List<MatchResult>> GetMatchResultsBySeasonAndCompetitionAndTeam(String season, String competition, String team);
 
-        List<MatchResult> GetMatchResultsBySeasonAndStadiumAndTeam(String season, String stadium, String team);
+        Task<List<MatchResult>> GetMatchResultsBySeasonAndStadiumAndTeam(String season, String stadium, String team);
 
-        List<MatchResult> GetMatchResultsByCompetitionAndStadiumAndTeam(String competition, String stadium, String team);
+        Task<List<MatchResult>> GetMatchResultsByCompetitionAndStadiumAndTeam(String competition, String stadium, String team);
 
-        List<MatchResult> GetMatchResultsBySeasonAndCompetition(String season, String competition);
+        Task<List<MatchResult>> GetMatchResultsBySeasonAndCompetition(String season, String competition);
 
-        List<MatchResult> GetMatchResultsBySeasonAndStadium(String season, String stadium);
+        Task<List<MatchResult>> GetMatchResultsBySeasonAndStadium(String season, String stadium);
 
-        List<MatchResult> getMatchResultsBySeasonAndTeam(String season, String team);
+        Task<List<MatchResult>> getMatchResultsBySeasonAndTeam(String season, String team);
 
-        List<MatchResult> GetMatchResultsByCompetitionAndStadium(String competition, String stadium);
+        Task<List<MatchResult>> GetMatchResultsByCompetitionAndStadium(String competition, String stadium);
 
-        List<MatchResult> GetMatchResultsByCompetitionAndTeam(String competition, String team);
+        Task<List<MatchResult>> GetMatchResultsByCompetitionAndTeam(String competition, String team);
 
-        List<MatchResult> GetMatchResultsByStadiumAndTeam(String stadium, String team);
+        Task<List<MatchResult>> GetMatchResultsByStadiumAndTeam(String stadium, String team);
 
-        List<MatchResult> GetMatchResultsBySeason(String season);
+        Task<List<MatchResult>> GetMatchResultsBySeason(String season);
 
-        List<MatchResult> GetMatchResultsByCompetition(String competition);
+        Task<List<MatchResult>> GetMatchResultsByCompetition(String competition);
 
-        List<MatchResult> GetMatchResultsByStadium(String stadium);
+        Task<List<MatchResult>> GetMatchResultsByStadium(String stadium);
 
-        List<MatchResult> GetMatchResultsByTeam(String team);
+        Task<List<MatchResult>> GetMatchResultsByTeam(String team);
 
-        List<MatchResult> GetMatchResultsByResult(String result);
+        Task<List<MatchResult>> GetMatchResultsByResult(String result);
 
-        List<MatchResult> GetAllMatchResults();
+        Task<List<MatchResult>> GetAllMatchResults();
 
-        List<string> GetTeamsPlayedAgainst();
-        List<string> GetTeamsPlayedAgainstBySeason(string season);
+        Task<List<string>> GetTeamsPlayedAgainst();
+        Task<List<string>> GetTeamsPlayedAgainstBySeason(string season);
 
-        List<string> GetTeamsPlayedAgainstBySeasonAndCompetition(string season, string competition);
+        Task<List<string>> GetTeamsPlayedAgainstBySeasonAndCompetition(string season, string competition);
 
-        List<string> GetTeamsPlayedAgainstByCompetition(string competition);
+        Task<List<string>> GetTeamsPlayedAgainstByCompetition(string competition);
 
-        List<string> GetTeamsPlayedAgainstBySeasonAndTeam(string season, string team);
+        Task<List<string>> GetTeamsPlayedAgainstBySeasonAndTeam(string season, string team);
 
-        List<string> GetSeasonsPlayedIn();
+        Task<List<string>> GetSeasonsPlayedIn();
 
-        List<string> GetCompetitionsPlayedIn();
+        Task<List<string>> GetCompetitionsPlayedIn();
 
-        List<string> GetCompetitionsPlayedInBySeason(string season);
+        Task<List<string>> GetCompetitionsPlayedInBySeason(string season);
 
-        List<string> GetCompetitionsPlayedInByTeam(string team);
+        Task<List<string>> GetCompetitionsPlayedInByTeam(string team);
 
 
-        List<string> GetRecord();
-        List<string> GetRecordbyTeam(string team);
+        Task<List<string>> GetRecord();
+       Task<List<string>> GetRecordbyTeam(string team);
 
     }
 }
